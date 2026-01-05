8984f43a596316a09f3f37e625eb17aed548a92b
Omar <ioomxr@icloud.com>
2026-01-05 17:01:23 +0200

    added a User model

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const campgrounds = require('./routes/campground.js')
 const reviews = require('./routes/review.js')
 const falsh  = require('connect-flash')
 const session = require('express-session')
+const passport= require('passport')
+const LocalStrategy = require('passport-local')
+const User = require('./models/user.js')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',  )
 const db = mongoose.connection;
@@ -25,7 +28,6 @@ app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-
 const sessionConfig = {
    secret: 'this should be a better secret',
    resave :false,
@@ -37,6 +39,11 @@ const sessionConfig = {
 }}
 app.use(session(sessionConfig))
 app.use(falsh())
+app.use(passport.initialize())
+app.use(passport.session())
+passport(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.use((req,res,next)=>{
    res.locals.success = req.flash('success');
