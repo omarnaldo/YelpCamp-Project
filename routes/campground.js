c06c17b8366cf28876516501c80cbde35b3ea7b7
Omar <ioomxr@icloud.com>
2026-01-08 21:53:12 +0200

    created chainable route handlers for a route path

--- routes/campground.js
+++ routes/campground.js
@@ -1,32 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync= require('../utils/catchAsync.js');
-const Campground = require('../models/campground.js');
 const {isLoggedIn} = require('../middleware.js')
 const {isAuthor} =require('../middleware.js')
 const {validateCampground} = require ('../middleware.js');
-const { campgroundSchema } = require('../schemas.js');
 const campgrounds = require('../controllers/campgrounds.js');
-const campground = require('../models/campground.js');
+
+router.route('/')
+ .get(catchAsync(campgrounds.index))
+ .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+
+router.get('/new',isLoggedIn, campgrounds.renderNewForm );
+
+router.route ('/:id')
+.get(catchAsync(campgrounds.showCampground))
+.put(validateCampground, isAuthor, catchAsync(campgrounds.editCampground))
+.delete( isAuthor,catchAsync(campgrounds.deleteCampground))
 
 
- router.get('/' , catchAsync(campgrounds.index));
 
-
- router.get('/new',isLoggedIn, campgrounds.renderNewForm );
-
-router.post('/', validateCampground, catchAsync(campgrounds.createCampground));
-
-
-router.get('/:id', catchAsync(campgrounds.showCampground));
 
 router.get('/:id/edit',isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
-
-router.put('/:id',validateCampground, isAuthor, catchAsync(campgrounds.editCampground))
-
-
-router.delete('/:id', isAuthor,catchAsync(campgrounds.deleteCampground))
 
 
 module.exports = router;
